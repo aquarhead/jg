15d2d477f4e992473c5ad4928af54f28f813d35b
Xhacker Liu <liu.dongyuan@gmail.com>
2013-12-04 20:37:27 +0800

    Sample image picker.

--- JikanGachou/JGStartPageViewController.m
+++ JikanGachou/JGStartPageViewController.m
@@ -9,9 +9,11 @@
 #import "JGStartPageViewController.h"
 #import <iCarousel.h>
 
-@interface JGStartPageViewController () <iCarouselDataSource, iCarouselDelegate>
+@interface JGStartPageViewController () <iCarouselDataSource, iCarouselDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet iCarousel *carousel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic) UIImagePickerController *imagePickerController;
 
 @end
 
@@ -27,17 +29,34 @@
     [self.carousel reloadData];
 }
 
-- (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel {
+- (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
     return 2;
 }
 
 - (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSUInteger)index reusingView:(UIView *)view
 {
-    NSString *imageName = [NSString stringWithFormat:@"start%lu", index+1];
+    NSString *imageName = [NSString stringWithFormat:@"start%u", index+1];
     NSLog(@"%@", imageName);
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:carousel.frame];
     imageView.image = [UIImage imageNamed:imageName];
     return imageView;
+}
+
+- (IBAction)buttonClicked:(id)sender
+{
+    [self showImagePicker];
+}
+
+- (void)showImagePicker
+{
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePickerController.delegate = self;
+    
+    self.imagePickerController = imagePickerController;
+    [self presentViewController:self.imagePickerController animated:YES completion:nil];
 }
 
 @end
