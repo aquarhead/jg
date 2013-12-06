7e6059326432c97f7b7a650edbc05b32b45c795b
Xhacker Liu <liu.dongyuan@gmail.com>
2013-12-06 19:16:44 +0800

    Image picker view WIP.

--- JikanGachou/JGImagePickerViewController.m
+++ JikanGachou/JGImagePickerViewController.m
@@ -9,11 +9,26 @@
 #import "JGImagePickerViewController.h"
 #import <AssetsLibrary/AssetsLibrary.h>
 
-@interface JGImagePickerViewController ()
+@interface JGImagePickerViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @end
 
 @implementation JGImagePickerViewController
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
 
 - (void)viewDidLoad
 {
