0e48ac0be53596b930e5d1c3d33ece1ad9d34b4c
Xhacker Liu <liu.dongyuan@gmail.com>
2013-12-15 23:28:06 +0800

    Edit page view controller.

---
+++ JikanGachou/JGEditPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGEditPageViewController.h
+//  JikanGachou
+//
+//  Created by Xhacker Liu on 12/15/13.
+//  Copyright (c) 2013 TeaWhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGEditPageViewController : UIViewController
+
+@end
