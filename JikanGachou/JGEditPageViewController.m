5f369587d05ed6196247ea8e0ea36df3acd88242
AquarHEAD Lou <aquarhead@gmail.com>
2014-01-01 14:03:16 +0800

    Add collectionView and copy tyk code.

--- JikanGachou/JGEditPageViewController.m
+++ JikanGachou/JGEditPageViewController.m
@@ -10,7 +10,7 @@
 
 @interface JGEditPageViewController ()
 
-@property (weak, nonatomic) IBOutlet UIView *pageView;
+@property (weak, nonatomic) IBOutlet UICollectionView *pagesCollectionView;
 
 @end
 
@@ -29,13 +29,66 @@
 {
     [super viewDidLoad];
 	
-    [self.pageView addSubview:[[[NSBundle mainBundle] loadNibNamed:@"EditPageCover" owner:self options:nil] firstObject]];
+//    [self.pageView addSubview:[[[NSBundle mainBundle] loadNibNamed:@"EditPageCover" owner:self options:nil] firstObject]];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)pageChanged:(UIPageControl *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CGPoint scrollTo = CGPointMake(kCategoryPageWidth * sender.currentPage, 0);
+    [self.categoryView setContentOffset:scrollTo animated:YES];
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    self.pageControl.currentPage = self.categoryView.contentOffset.x / kCategoryPageWidth;
+}
+
+#pragma mark Collection View
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.numberOfPages * self.numberOfIconsInPage;
+}
+
+- (void)configureCell:(TYKCategoryCell *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    TYKCategory *category;
+    if (indexPath.row < self.categories.count) {
+        category = self.categories[indexPath.row];
+    }
+
+    cell.category = category;
+    if ([self.selectedCategory isEqual:category]) {
+        cell.pressed = YES;
+    }
+    else {
+        cell.pressed = NO;
+    }
+    cell.label.text = category.name;
+    if (category.icon) {
+        cell.icon.image = [UIImage imageNamed:category.icon];
+    }
+    else {
+        cell.icon.image = nil;
+    }
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
+
+    [self configureCell:(TYKCategoryCell *)cell atIndexPath:indexPath];
+
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+
+    self.selectedCategory = ((TYKCategoryCell *)cell).category;
+    [self.categoryView reloadData];
 }
 
 @end
